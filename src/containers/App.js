81a3814f5bbe782bff0d4e52aac080dca37e0c23
bkchu <brandon.k.chung@gmail.com>
2018-01-22 14:44:08 -0600

    lecture 87 completed

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,15 +4,61 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: "awef1", name: "Brandon", age: 23 },
-      { id: "awef2", name: "Kevin", age: 26 },
-      { id: "awef3", name: "June", age: 22 }
-    ],
-    otherState: "some other value",
-    showPersons: false
-  };
+  constructor(props) {
+    super(props);
+    console.log("[App.js] Inside Constructor", props);
+    this.state = {
+      persons: [
+        { id: "awef1", name: "Brandon", age: 23 },
+        { id: "awef2", name: "Kevin", age: 26 },
+        { id: "awef3", name: "June", age: 22 }
+      ],
+      otherState: "some other value",
+      showPersons: false
+    };
+  }
+
+  componentWillMount() {
+    console.log("[App.js] Inside componentWillMount()");
+  }
+
+  componentDidMount() {
+    console.log("[App.js] Inside componentDidMount()");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      "[UPDATE App.js] Inside shouldComponentUpdate()",
+      nextProps,
+      nextState
+    );
+    return (
+      nextState.persons !== this.state.persons ||
+      nextState.showPersons !== this.state.showPersons
+    );
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      "[UPDATE App.js] Inside componentWillUpdate()",
+      nextProps,
+      nextState
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("[UPDATE App.js] Inside componentDidUpdate()");
+  }
+
+  // state = {
+  //   persons: [
+  //     { id: "awef1", name: "Brandon", age: 23 },
+  //     { id: "awef2", name: "Kevin", age: 26 },
+  //     { id: "awef3", name: "June", age: 22 }
+  //   ],
+  //   otherState: "some other value",
+  //   showPersons: false
+  // };
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -44,6 +90,8 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] Inside render()");
+
     let persons = null;
 
     if (this.state.showPersons) {
@@ -58,7 +106,15 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+        <button
+          onClick={() => {
+            this.setState({ showPersons: true });
+          }}
+        >
+          Show Persons
+        </button>
         <Cockpit
+          appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
